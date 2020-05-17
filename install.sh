854e122b616c974df063bc6fe1196f93c2828768
vcfe <anenui@qq.com>
2020-05-17 12:59:23 +0800

    First Commit

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+wget https://github.com/vcfe/gd/raw/master/gd.sh
+echo "输入配置gclone的名称"
+read -p "gclone config Name:" gclone
+sed -i "s/goog/$gclone/g" gd.sh
+chmod +x gd.sh
+echo "请输入 ./gd.sh 使用脚本"
