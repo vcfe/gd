75819c486742773ad41c4d1febe459c6b745326c
vcfe <anenui@qq.com>
2020-05-19 13:22:21 +0800

    First Commit

--- gd.sh
+++ gd.sh
@@ -1,45 +1,28 @@
 #!/bin/bash
-echo "输入分享链接"
-read -p "请输入:" link
-link=${link#*id=};
-link=${link#*folders/}
-echo $link
-link=${link#*d/}
-link=${link%?usp*}
-echo -e "1   选择目录"
-echo -e "2   创建目录"
-read -p " 选择保存   >: " list
-#copylink=`gclone copy goog:{$link} goog:$dyj --drive-server-side-across-configs -v`
-case $list in
-	1)
-	gclone lsd goog:|awk '{print NR,$5}'
-	read -p "选择" option
-	dyj=`gclone lsd goog:|awk -v sz=$option 'NR==sz {print$5}'`
-	while true
-	do
-	    clear
-	    echo -e "输入n浏览下一级目录\n输入y将保存到 $dyj 文件夹\n输入其他字符将在 $dyj 下创建文件夹"
-    	read -p "保存到 $dyj 这个文件夹？" suer
-        case $suer in
-            n)
-            gclone lsd goog:$dyj | awk '{print NR,$5}'
-            read -p "选择" option
-            dyj2=`gclone lsd goog:$dyj|awk -v sz=$option 'NR==sz {print$5}'`
-            dyj=$dyj"/"$dyj2
-            ;;
-            y)
-            gclone copy goog:{$link} goog:$dyj --drive-server-side-across-configs -v
-            break
-            ;;
-            *)
-            gclone copy goog:{$link} goog:$dyj"/"$suer --drive-server-side-across-configs -v
-            break
-            ;;
-        esac
-    done
-	;;
-	2)
-	read -p "创建目录:" mkdir
-	gclone copy goog:{$link} goog:$mkdir --drive-server-side-across-configs -v
-esac
+copy(){
+gclone copy google:{$1} google:$2 --drive-server-side-across-configs -v
+}
+folder(){
+gclone lsd google:$2|awk -v sz=$1 'NR==sz {print$5}'
+}
 
+read -p """输入分享链接
+     请输入 ~>:""" link
+link=${link#*id=};link=${link#*folders/};link=${link#*d/};link=${link%?usp*}
+
+echo -e "为了操作快捷,简便,只支持选择一级目录\n二级目录需要手动创建"
+gclone lsd google:|awk '{print "     ",NR,"     ",$5}'
+echo -e "确认请回车\n\n输入其他字符将在此目录下创建新文件夹并copy\n"
+read -p "     选择文件夹 ~>: " list
+case $list in
+    [1-9][0-9])
+        name=`folder $list`
+        #gclone lsd google:$name|awk '{print NR,$5}'
+        echo -e "确认请回车\n\n输入其他字符将在此 $name 下创建新文件夹并copy\n"
+        read -p "保存到 $name 这个文件夹？" list2
+        copy $link $name"/"$list2
+        ;;
+        *)
+        gclone copy google:{$link} google:$list --drive-server-side-across-configs -v
+esac
+        
