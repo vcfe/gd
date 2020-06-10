cf81851fb9ac1f5618b96818fa0ddbca4d02f56b
vcfe <anenui@qq.com>
2020-06-10 10:42:22 +0800

    加入数据比对和去重

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 rm -rf gd.sh
 wget https://github.com/vcfe/gd/raw/master/gd.sh
-echo "输入配置gclone的名称"
+clear
+echo -e "\n\n\n\n\n输入配置gclone的名称"
 read -p "gclone config Name:" gclone
 sed -i "s/goog/$gclone/g" gd.sh
 chmod +x gd.sh
@@ -9,6 +10,6 @@ dir=`pwd`
 pa=${PATH%%:*}
 rm -rf $pa/gd
 ln -s $dir/gd.sh $pa/gd
-clear
-echo -e "\n\n\n\n\n\n\n配合screen使用效果更佳(可以后台执行)\n\n\n输入 gd 使用脚本"
+echo -e "\n\n\n\n\n\n\n配合screen使用效果更佳(可以后台执行)"
+echo -e "\n\n\n输入 gd 使用脚本"
 
